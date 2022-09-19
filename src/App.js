9ba8f11583749eebcf00515b8e2bd98e877c15d0
Farida Fadilah <faridafadilah42807@gmail.com>
2022-09-19 10:36:09 +0700

    Membuat HomePage React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <Header />
+      <div className='container'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>BootCamp Batch #3</h1>
+        <h3>Farida Fadilah</h3>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// function renderDOM(content, id) {
+//   ReactDOM.render(content, document.getElementById(id));
+// }
+
+// const element = (<div>
+//   <h1>Farida Fadilah</h1>
+//   <h2>Bootcamp Batch #3</h2>
+// </div>);
+
+// renderDOM(element, "root");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
