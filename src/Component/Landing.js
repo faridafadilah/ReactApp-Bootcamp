6f4731bc28ae9c36cd479f6bc72f7c91eabb54ab
Farida Fadilah <faridafadilah42807@gmail.com>
2022-09-19 10:51:55 +0700

    Mengupdate content

--- src/Component/Landing.js
+++ src/Component/Landing.js
@@ -1,10 +1,8 @@
 import React from "react";
-import logo from './logo.svg';
 
 function Landing() {
     return (
         <div className='container'>
-            <img src={logo} className="App-logo" alt="logo" />
             <h1>BootCamp Batch #3</h1>
             <h3>Farida Fadilah</h3>
         </div>
