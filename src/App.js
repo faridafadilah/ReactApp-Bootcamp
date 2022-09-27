8f2d7959b1cb87f452e388d1249979a9a7e74168
Farida Fadilah <faridafadilah42807@gmail.com>
2022-09-27 14:10:55 +0700

    Membuat Form Input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
-import Clock from "./Component/Clock";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event, field) {
+    this.setState({ [field]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(`First Name: ${this.state.firstName} | Last Name: ${this.state.lastName}`);
+    event.preventDefault();
+  }
   render() {
     return (
-      <>
-      {/* Memanggil Komponen Clock */}
-      <Clock />
-      </>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" name="firstName" value={this.state.firstName} onChange={(event)=>this.handleChange(event, "firstName")} />
+        </label>
+        <label>
+          Last Name:
+          <input type="text" name="lastName" value={this.state.lastName} onChange={(event)=>this.handleChange(event, "lastName")} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
